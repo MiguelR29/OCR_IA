7f2733ae5b184b8fa40608a4cd2699a90fe32c0b
MiguelR29 <miguelromamigazo4@gmail.com>
2024-05-28 21:08:28 -0600

    cambio en el ROI y learn matrix

--- ROI.py
+++ ROI.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 from PIL import Image
 
 # Ruta de las imágenes de entrenamiento
-train_path = r'C:\Universidad\Sexto_Semestre\IA\Proyecto_final\Imagenes_entrenamiento'
+train_path = r'Imagenes_entrenamiento'
 
 # Obtener la lista de archivos de imágenes en el directorio de entrenamiento
 image_files = [os.path.join(train_path, f) for f in os.listdir(train_path) if f.endswith('.jpg')]
@@ -28,8 +28,19 @@ for image_file in image_files:
 accumulated_image /= len(image_files)
 
 print(f"Se han acumulado {len(image_files)} imágenes de entrenamiento.")
-# Mostrar la imagen acumulada
+print("Matriz normalizada acumulada:")
+print(accumulated_image)
+
+# Mostrar la imagen acumulada con la ROI
 plt.imshow(accumulated_image, cmap='hot')
 plt.colorbar()
 plt.title('Superposición de Imágenes de Entrenamiento')
+
+# Dibujar la ROI en la imagen
+roi_y_range = range(7, 21)
+roi_x_range = range(4, 23)
+for y in roi_y_range:
+    for x in roi_x_range:
+        plt.plot(x, y, 'bo', markersize=2)
+
 plt.show()
